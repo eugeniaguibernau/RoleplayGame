ba6b9d29f1d877b0e0bb3a1be76432f7e66713a9
FelipePaladino407 <felipaladino05@gmail.com>
2024-09-11 18:29:52 -0300

    Añado la clase Mago y LibroMagico, que colabora con Mago¨

---
+++ src/Program/Mago.cs
@@ -0,0 +1,114 @@
+using System.Runtime.CompilerServices;
+
+namespace Program;
+
+public class Mago
+{
+    private string nombre;
+    public string Nombre { get; set; }
+    
+
+    private double vida;
+    public double Vida { get; set; }
+
+    private List<Elemento> ListaItems;
+
+
+    private LibroMagico libroDeHechizos;
+    public LibroMagico LibroDeHechizos => libroDeHechizos;
+    
+
+    public Mago(string nombre, double vida)
+    {
+        Nombre = nombre;
+        Vida = vida;
+    }
+    
+    
+
+
+    public string ObtenerNombre()             // Metodo para devolver el nombre del Magucho.
+    {
+        return Nombre;
+        
+    }
+
+    public double ObtenerVida()               // Metodo para devolver el valor de vida del Magucho.
+    {
+        return Vida;
+    }
+
+    public double ObtenerValorDefensa()
+    {
+        double defensa = 0;
+        foreach (Elemento item in ListaItems)
+        {
+            defensa += item.Defensa;
+        }
+
+        return defensa;
+    }
+    
+
+    public void RecibirAtaqueEnano(Enano enano)
+    {
+        Vida -= enano.ObtenerValorDeAtaque() * this.ObtenerValorDefensa()/100;
+        if (Vida <= 0)
+        {
+            Vida = 0;
+            Console.WriteLine($"Tu mago {Nombre} ha recibido {enano.ObtenerValorDeAtaque()} puntos de damage y ha muerto.");
+        }
+        Console.WriteLine($"Tu mago {Nombre} ha recibido {enano.ObtenerValorDeAtaque()} puntos de damage y le quedan {Vida} puntos de vida. Ojo con Gaspar!");
+    }
+
+    
+    public void RecibirAtaqueElfo(Elfo elfo)
+    {
+        Vida -= elfo.ObtenerValorDeAtaque() * this.ObtenerValorDefensa()/100;
+        if (Vida <= 0)
+        {
+            Vida = 0;
+            Console.WriteLine($"Tu mago {Nombre} ha recibido {elfo.ObtenerValorDeAtaque()} puntos de damage y ha muerto.");
+        }
+        Console.WriteLine($"Tu mago {Nombre} ha recibido {elfo.ObtenerValorDeAtaque()} puntos de damage y le quedan {Vida} puntos de vida. Ojo con Gaspar!");
+    }
+    
+    
+    public void RecibirAtaqueMago(Mago mago)
+    {
+        Vida -= this.ObtenerValorDeAtaque() * this.ObtenerValorDefensa()/100;
+        if (Vida <= 0)
+        {
+            Vida = 0;
+            Console.WriteLine($"Tu mago {Nombre} ha recibido {this.ObtenerValorDeAtaque()} puntos de damage y ha muerto.");
+        }
+        Console.WriteLine($"Tu mago {Nombre} ha recibido {this.ObtenerValorDeAtaque()} puntos de damage y le quedan {Vida} puntos de vida. Ojo con Gaspar!");
+    }
+
+    
+    public void Curar()                                                
+    {
+        if (Vida <= Vida - 15)
+        {
+            Vida += 15;
+            Console.WriteLine($"{Nombre} ha recuperado {15} puntos de salud. Enhorabuena!, pero ojito con Gaspar!");
+        }
+        
+        Console.WriteLine($"No puedes curarte, pues ya tienas mucha vida.");
+    }
+    
+
+    public double ObtenerValorDeAtaque()
+    {
+        double poderItems = 0;
+        foreach (Elemento item in ListaItems)
+        {
+            poderItems += item.Ataque;                                  // Suma el valor de ataque de cada ítem
+        }
+        
+        double poderHechizos = LibroDeHechizos.ObtenerPoderTotal();    // El poder sumado de todos los hechizos.
+        
+        return poderItems + poderHechizos;                             // Aca estaria retornando el poder total maximo que lanza el mago.
+    }
+
+}    
