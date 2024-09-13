43f17432b464c7fabb922da5bf271ce60b4d163a
Santiago Blanco <santiago.blancoc@correo.ucu.edu.uy>
2024-09-13 10:21:11 -0300

    Agregar TestEnanoAtacaElfo

--- LibraryTests/ElfoTests.cs
+++ LibraryTests/ElfoTests.cs
@@ -1,102 +1,108 @@
-namespace LibraryTests;
-
-public class ElfoTests
+namespace LibraryTests
 {
-    [Test]
-    public void TestObtenerValorAtaque()
+    public class ElfoTests
     {
-        // Crear elementos
-        var elemento1 = new Elementos("Espada de chocolate", 15, 2, "Espada");
-        var elemento2 = new Elementos("Rifle de caza con gatillo derretido", 10, 3, "Arma larga");
+        [Test]
+        public void TestObtenerValorAtaque()
+        {
+            // Crear elementos
+            var elemento1 = new Elementos("Espada de chocolate", 15, 2, "Espada");
+            var elemento2 = new Elementos("Rifle de caza con gatillo derretido", 10, 3, "Arma larga");
 
-        // Crear elfo y agregar elementos
-        var elfo = new Elfo("Elfo pedante", new List<Elementos> { elemento1 }, 100);
-        elfo.AgregarElemento(elemento2);
+            // Crear elfo y agregar elementos
+            var elfo = new Elfo("Elfo pedante", new List<Elementos> { elemento1 }, 100);
+            elfo.AgregarElemento(elemento2);
 
-        // Verificar que el ataque total es la suma correcta
-        Assert.That(elfo.ObtenerValorDeAtaque(), Is.EqualTo(25));
-    }
+            // Verificar que el ataque total es la suma correcta
+            Assert.That(elfo.ObtenerValorDeAtaque(), Is.EqualTo(25));
+        }
 
-    [Test]
-    public void TestObtenerValorDefensa()
-    {
-        // Crear elementos
-        var elemento1 = new Elementos("Escudo", 0, 10, "Defensa");
-        var elemento2 = new Elementos("Armadura", 0, 5, "Protección");
+        [Test]
+        public void TestObtenerValorDefensa()
+        {
+            // Crear elementos
+            var elemento1 = new Elementos("Escudo", 0, 10, "Defensa");
+            var elemento2 = new Elementos("Armadura", 0, 5, "Protección");
 
-        // Crear elfo y agregar elementos
-        var elfo = new Elfo("Elfo2", new List<Elementos> { elemento1 }, 100);
-        elfo.AgregarElemento(elemento2);
+            // Crear elfo y agregar elementos
+            var elfo = new Elfo("Elfo2", new List<Elementos> { elemento1 }, 100);
+            elfo.AgregarElemento(elemento2);
 
-        // Verificar que la defensa total es la suma correcta
-        Assert.That(elfo.ObtenerValorDefensa(), Is.EqualTo(15));
-    }
+            // Verificar que la defensa total es la suma correcta
+            Assert.That(elfo.ObtenerValorDefensa(), Is.EqualTo(15));
+        }
 
-    [Test]
-    public void TestElfoAtacaElfo()
-    {
-        // Crear elfo atacante
-        var elementoAtaque = new Elementos("Espada", 20, 5, "Arma");
-        var elfoAtacante = new Elfo("Elfo Atacante enojado", new List<Elementos> { elementoAtaque }, 100);
+        [Test]
+        public void TestElfoAtacaElfo()
+        {
+            // Crear elfo atacante
+            var elementoAtaque = new Elementos("Espada", 20, 5, "Arma");
+            var elfoAtacante = new Elfo("Elfo Atacante enojado", new List<Elementos> { elementoAtaque }, 100);
 
-        // Crear elfo defensor
-        var elementoDefensa = new Elementos("Escudo", 5, 10, "Defensa");
-        var elfoDefensor = new Elfo("ElfoDefensor", new List<Elementos> { elementoDefensa }, 90);
+            // Crear elfo defensor
+            var elementoDefensa = new Elementos("Escudo", 5, 10, "Defensa");
+            var elfoDefensor = new Elfo("ElfoDefensor", new List<Elementos> { elementoDefensa }, 100);
 
-        // Elfo atacante ataca al elfo defensor
-        elfoDefensor.RecibirAtaqueElfo(elfoAtacante);
+            // Elfo atacante ataca al elfo defensor
+            elfoDefensor.RecibirAtaqueElfo(elfoAtacante);
 
-        // Verificar la vida restante
-        double expectedDano = CalcularDano(elfoAtacante, elfoDefensor);
-        double expectedVida = 90 - expectedDano;
-        Assert.That(elfoDefensor.Vida, Is.EqualTo(expectedVida));
-    }
+            // Verificar la vida restante
+            double expectedDano = Math.Max(elfoAtacante.ObtenerValorDeAtaque() - elfoDefensor.ObtenerValorDefensa(), 0);
+            double expectedVida = 100 - expectedDano;
+            Assert.That(elfoDefensor.Vida, Is.EqualTo(expectedVida));
+        }
 
-    [Test]
-    public void TestMagoAtacaElfo()
-    {
-        // Crear elfo
-        var elementoElfo = new Elementos("Armadura", 0, 5, "Protección");
-        var elfo = new Elfo("Elfo1", new List<Elementos> { elementoElfo }, 100);
+        [Test]
+        public void TestMagoAtacaElfo()
+        {
+            // Crear elfo
+            var elementoElfo = new Elementos("Armadura", 0, 5, "Protección");
+            var elfo = new Elfo("Elfo1", new List<Elementos> { elementoElfo }, 100);
 
-        // Crear mago
-        var elementoMago = new Elementos("Palo de madera con brillo", 30, 0, "Magia");
-        var mago = new Mago("Mago1", new List<Elementos> { elementoMago }, new List<Hechizos>(), 100);
+            // Crear mago
+            var elementoMago = new Elementos("Palo de madera con brillo", 30, 0, "Magia");
+            var mago = new Mago("Mago1", new List<Elementos> { elementoMago }, new List<Hechizos>(), 100);
 
-        // Mago ataca al elfo
-        elfo.RecibirAtaqueMago(mago);
+            // Mago ataca al elfo
+            elfo.RecibirAtaqueMago(mago);
 
-        // Verificar vida restante del elfo
-        double expectedDano = CalcularDano(mago, elfo);
-        double expectedVida = 100 - expectedDano;
-        Assert.That(elfo.Vida, Is.EqualTo(expectedVida));
-    }
+            // Verificar vida restante del elfo
+            double expectedDano = Math.Max(mago.ObtenerValorDeAtaque() - elfo.ObtenerValorDefensa(), 0);
+            double expectedVida = 100 - expectedDano;
+            Assert.That(elfo.Vida, Is.EqualTo(expectedVida));
+        }
 
-    [Test]
-    public void TestElfoSeCura()
-    {
-        // Crear elfo con poca vida
-        var elfoDebil = new Elfo("ElfoDebilucho", new List<Elementos>(), 20);
+        [Test]
+        public void TestEnanoAtacaElfo()
+        {
+            // Crear elfo
+            var elementoElfo2 = new Elementos("Pechera de hierro", 0, 10, "Protección");
+            var elfo2 = new Elfo("Elfo2", new List<Elementos> { elementoElfo2 }, 100);
 
-        // Elfo se cura
-        elfoDebil.Curar();
+            // Crear enano
+            var elementoEnano = new Elementos("Tijera", 20, 0, "Herramienta");
+            var enano = new Enanos("Enano gordo", new List<Elementos> { elementoEnano }, 100);
 
-        // Verificar que la vida aumentó correctamente (+10)
-        Assert.That(elfoDebil.Vida, Is.EqualTo(30));
-    }
+            // Enano ataca al elfo
+            elfo2.RecibirAtaqueEnano(enano);
 
-    private double CalcularDano(Enanos enanoAtacante, Elfo elfoAtacado)
-    {
-        return Math.Max(enanoAtacante.ObtenerValorDeAtaque() - elfoAtacado.ObtenerValorDefensa(), 0);
-    }
+            // Verificar vida restante del elfo
+            double expectedDano = Math.Max(enano.ObtenerValorDeAtaque() - elfo2.ObtenerValorDefensa(), 0);
+            double expectedVida = 100 - expectedDano;
+            Assert.That(elfo2.Vida, Is.EqualTo(expectedVida));
+        }
 
-    private double CalcularDano(Elfo elfoAtacante, Elfo elfoAtacado)
-    {
-        return Math.Max(elfoAtacante.ObtenerValorDeAtaque() - elfoAtacado.ObtenerValorDefensa(), 0);
-    }
+        [Test]
+        public void TestElfoSeCura()
+        {
+            // Crear elfo con poca vida
+            var elfoDebil = new Elfo("ElfoDebilucho", new List<Elementos>(), 20);
 
-    private double CalcularDano(Mago magoAtacante, Elfo elfoAtacado)
-    {
-        return Math.Max(magoAtacante.ObtenerValorDeAtaque() - elfoAtacado.ObtenerValorDefensa(), 0);
+            // Elfo se cura
+            elfoDebil.Curar();
+
+            // Verificar que la vida aumentó correctamente (+10)
+            Assert.That(elfoDebil.Vida, Is.EqualTo(30));
+        }
     }
 }
